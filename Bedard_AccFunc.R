2e33159f1690510eb2622b3cf51e8d4212588773
Sanket Agrawal <62806894+babasanku@users.noreply.github.com>
2021-01-20 16:34:56 +0530

    Update Bedard_AccFunc.R

--- Bedard_AccFunc.R
+++ Bedard_AccFunc.R
@@ -13,5 +13,16 @@ for(i in 1:length(h)){
 
 plot(h, lhat, type = "l")
 abline(h = 2.46)
+abline(v = 1.9)
 plot(h, aoar, type = "l")
 abline(h = 0.159)
+abline(v = 1.9)
+
+s <- seq(0.1, 100, length.out = 1e4)
+h <- 1.9
+barker <- s/(1+s)
+bedard <- pnorm((log(s) - h/2)/sqrt(h)) + s*pnorm((-log(s) - h/2)/sqrt(h))
+
+plot(s, barker, type = "l", col = "blue", ylab="acceptance prob.")
+lines(s, bedard, col = "red")
+legend("bottomright", legend = c("Barker", "Bedard (h = 1.9)"), col = c("blue", "red"), lty = 1)
